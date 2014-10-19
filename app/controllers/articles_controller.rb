88269f0757991bef35588fa68f9e1da95be2faae
Anna Ho <annaho@ecoworkinc.com>
2014-10-19 17:33:08 +0800

    fetch article's titles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+  
+  def index
+    @articles = fetch_article
+  end
+  
+  private
+  
+  def fetch_article
+    article = File.read('tmp/articles.xml')
+    articles = article.scan(/<title>(.+)<\/title>\s+<link>.+<\/link>\s+<comments>.+<\/comments>\s+<pubDate>(.+)<\/pubDate>/)
+    articles.sort_by!{|a,b|a[1].to_time <=> b[1].to_time}
+  end
+end
